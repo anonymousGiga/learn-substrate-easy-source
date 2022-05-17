06a3f0c43dfcee480c6568b8aadd0f3341e8cc38
令狐一冲 <43949039+anonymousGiga@users.noreply.github.com>
2022-05-17 17:02:22 +0800

    Delete traits/ext4/target directory

--- traits/ext4/target/debug/deps/ext4-8fa4f32becd02873.d
+++
@@ -1,5 +0,0 @@
-/home/andy/Source/learn/learn-substrate-easy-source/traits/ext4/target/debug/deps/ext4-8fa4f32becd02873: src/main.rs
-
-/home/andy/Source/learn/learn-substrate-easy-source/traits/ext4/target/debug/deps/ext4-8fa4f32becd02873.d: src/main.rs
-
-src/main.rs:
--- traits/ext4/target/debug/ext4.d
+++
@@ -1,1 +0,0 @@
-/home/andy/Source/learn/learn-substrate-easy-source/traits/ext4/target/debug/ext4: /home/andy/Source/learn/learn-substrate-easy-source/traits/ext4/src/main.rs
