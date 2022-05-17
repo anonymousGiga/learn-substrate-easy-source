6221d7862bc35373374bd297eb6706ca3baa7f22
令狐一冲 <43949039+anonymousGiga@users.noreply.github.com>
2022-05-17 17:01:58 +0800

    Delete traits/ext2/target directory

--- traits/ext2/target/debug/deps/ext2-f4532e1429642dda.d
+++
@@ -1,5 +0,0 @@
-/home/andy/Source/learn/learn-substrate-easy-source/traits/ext2/target/debug/deps/ext2-f4532e1429642dda: src/main.rs
-
-/home/andy/Source/learn/learn-substrate-easy-source/traits/ext2/target/debug/deps/ext2-f4532e1429642dda.d: src/main.rs
-
-src/main.rs:
