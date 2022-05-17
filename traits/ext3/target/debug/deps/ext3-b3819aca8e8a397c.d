7198e25d1efae800eef9b6e6c969633cae6e2d1e
令狐一冲 <43949039+anonymousGiga@users.noreply.github.com>
2022-05-17 17:02:11 +0800

    Delete traits/ext3/target directory

--- traits/ext3/target/debug/deps/ext3-b3819aca8e8a397c.d
+++
@@ -1,5 +0,0 @@
-/home/andy/Source/learn/learn-substrate-easy-source/traits/ext3/target/debug/deps/ext3-b3819aca8e8a397c: src/main.rs
-
-/home/andy/Source/learn/learn-substrate-easy-source/traits/ext3/target/debug/deps/ext3-b3819aca8e8a397c.d: src/main.rs
-
-src/main.rs:
--- traits/ext3/target/debug/ext3.d
+++
@@ -1,1 +0,0 @@
-/home/andy/Source/learn/learn-substrate-easy-source/traits/ext3/target/debug/ext3: /home/andy/Source/learn/learn-substrate-easy-source/traits/ext3/src/main.rs
