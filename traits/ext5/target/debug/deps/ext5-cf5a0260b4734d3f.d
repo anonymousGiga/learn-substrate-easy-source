a426eb63bf49fb1f755772136769a423fd07a0be
令狐一冲 <43949039+anonymousGiga@users.noreply.github.com>
2022-05-17 17:02:32 +0800

    Delete traits/ext5/target directory

--- traits/ext5/target/debug/deps/ext5-cf5a0260b4734d3f.d
+++
@@ -1,5 +0,0 @@
-/home/andy/Source/learn/learn-substrate-easy-source/traits/ext5/target/debug/deps/ext5-cf5a0260b4734d3f: src/main.rs
-
-/home/andy/Source/learn/learn-substrate-easy-source/traits/ext5/target/debug/deps/ext5-cf5a0260b4734d3f.d: src/main.rs
-
-src/main.rs:
--- traits/ext5/target/debug/ext5.d
+++
@@ -1,1 +0,0 @@
-/home/andy/Source/learn/learn-substrate-easy-source/traits/ext5/target/debug/ext5: /home/andy/Source/learn/learn-substrate-easy-source/traits/ext5/src/main.rs
